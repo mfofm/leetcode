8ec38ccbc1dafd9d56f391a2e3ba2f8a124a7c6c
mengfan1018 <mengfan9210@outlook.com>
2018-12-18 08:20:43 +0800

    add 038

---
+++ 001-100/038_countandSay.py
@@ -0,0 +1,37 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+Note: Each term of the sequence of integers will be represented as a string.
+"""
+
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s='1'
+        for i in range(2,n+1):
+            s=self.count(s)
+        return s
+    def count(self,s):
+        t=''; count=0; curr='#'
+        for i in s:
+            if i!=curr:
+                if curr!='#':
+                    t+=str(count)+curr
+                curr=i
+                count=1
+            else:
+                count+=1
+        t+=str(count)+curr
+        return t
