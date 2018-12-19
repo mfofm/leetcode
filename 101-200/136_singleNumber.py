fe0686a7e172a38987e66d44e1d554b403b84a32
mengfan1018 <mengfan9210@outlook.com>
2018-12-19 08:05:31 +0800

    add 136

---
+++ 101-200/136_singleNumber.py
@@ -0,0 +1,28 @@
+"""
+Given a non-empty array of integers, 
+every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. 
+Could you implement it without using extra memory?
+
+Example 1:
+	Input: [2,2,1]
+	Output: 1
+Example 2:
+	Input: [4,1,2,1,2]
+	Output: 4
+"""
+
+
+# 感觉这个方法还是比较巧妙的
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            res^=i
+        return res
