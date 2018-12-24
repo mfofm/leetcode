84078ea0ffff8889f2fb77a342476a654245de1e
mengfan1018 <mengfan9210@outlook.com>
2018-12-24 08:25:49 +0800

    add 371

---
+++ 301-400/371_sumifTwoIntegers.py
@@ -0,0 +1,22 @@
+"""
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+	Input: a = 1, b = 2
+	Output: 3
+Example 2:
+	Input: a = -2, b = 3
+	Output: 1
+"""
+
+
+# 偷懒了
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        add_temp = [a, b]
+        return sum(add_temp)
