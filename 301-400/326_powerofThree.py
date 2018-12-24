e9d6914501356ca1c7a392cbe97c847d83dc5857
mengfan1018 <mengfan9210@outlook.com>
2018-12-24 08:12:38 +0800

    add 326

---
+++ 301-400/326_powerofThree.py
@@ -0,0 +1,34 @@
+"""
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+	Input: 27
+	Output: true
+Example 2:
+	Input: 0
+	Output: false
+Example 3:
+	Input: 9
+	Output: true
+Example 4:
+	Input: 45
+	Output: false
+
+Follow up:
+	Could you do it without using any loop / recursion?
+"""
+
+class Solution:
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        else:
+            s = str(math.log(n,3))
+            if s[-2] == '.':  
+                return True
+            else:
+                return False
