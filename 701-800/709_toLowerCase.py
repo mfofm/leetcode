f15491fa83f66d6cba407328d91e93e6e972d482
mengfan1018 <mengfan9210@outlook.com>
2018-12-26 16:45:25 +0800

    add 709

---
+++ 701-800/709_toLowerCase.py
@@ -0,0 +1,24 @@
+"""
+Implement function ToLowerCase() that has a string parameter str, and returns the same 
+string in lowercase.
+
+Example 1:
+	Input: "Hello"
+	Output: "hello"
+Example 2:
+	Input: "here"
+	Output: "here"
+Example 3:
+	Input: "LOVELY"
+	Output: "lovely"
+"""
+
+
+# 手动狗头
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
