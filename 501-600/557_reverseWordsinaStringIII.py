eabd3c3ea9527de214e2289126d72d4aecc49140
mengfan1018 <mengfan9210@outlook.com>
2018-12-26 08:16:35 +0800

    add 557

---
+++ 501-600/557_reverseWordsinaStringIII.py
@@ -0,0 +1,23 @@
+"""
+Given a string, you need to reverse the order of characters in each word within a 
+sentence while still preserving whitespace and initial word order.
+
+Example 1:
+	Input: "Let's take LeetCode contest"
+	Output: "s'teL ekat edoCteeL tsetnoc"
+
+Note: In the string, each word is separated by single space and there will not be 
+	any extra space in the string.
+"""
+
+
+class Solution:
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        temp = s.split(" ")
+        for x in range(0, len(temp)):
+            temp[x] = temp[x][::-1]
+        return " ".join(temp)
