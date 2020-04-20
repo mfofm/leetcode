909608bff6677a7e927c7a88d181ee896f90d8d1
mengfan <meng_fan_@outlook.com>
2020-04-20 13:33:11 +0800

    add 1221

---
+++ 1200-1299/1221_SplitaStringinBalancedStrings.py
@@ -0,0 +1,54 @@
+"""
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] = 'L' or 'R'
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/split-a-string-in-balanced-strings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        result = 0
+        countL, countR = 0, 0
+        for item in s:
+            if item == 'R':
+                countR += 1
+            elif item == 'L':
+                countL += 1
+            if countL == countR:
+                result += 1
+        return result
